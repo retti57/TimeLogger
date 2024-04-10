f9b91b430d07ae34bd44e3d857665e7502ae7a5d
Czaja <dczajka1@gmail.com>
2024-04-10 11:33:10 +0200

    zainstalowanie aplikacji

--- time_logger_project/settings.py
+++ time_logger_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'time_logger_backend_app',
+    'time_logger_frontend_app'
 ]
 
 MIDDLEWARE = [
