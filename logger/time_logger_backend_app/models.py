f534df0087ea4f30c779c28e8f06c2ce8f7a06e7
Czaja <dczajka1@gmail.com>
2024-04-13 09:30:05 +0200

    model Aircraft

--- logger/time_logger_backend_app/models.py
+++ logger/time_logger_backend_app/models.py
@@ -5,24 +5,52 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class MilPerson(models.Model):
+    class Meta:
+        verbose_name = 'Personel'
+        verbose_name_plural = 'Personel latający'
+
+    CHOICES = (
+        ("pilot", "PILOT"),
+        ("technician", "TECHNIK"),
+        ("gunner", "STRZELEC"),
+        ("medic", "RATOWNIK"),
+        ("other", "INNY"),
+    )
+
     rank = models.CharField(max_length=30, blank=False)
     first_name = models.CharField(max_length=30, blank=False)
     last_name = models.CharField(max_length=30, blank=False)
-    # func_on_board = models.ForeignKey(to=FunctionOnBoard,on_delete=models.CASCADE,related_name='users')
-    user = models.ForeignKey(to=User,on_delete=models.CASCADE, blank=True, related_name='credentials')
-#
-#
-# class FunctionOnBoard(models.Model):
-#     CHOICES = (
-#         ("pilot", "PILOT"),
-#         ("technician", "TECHNIK"),
-#         ("gunner", "STRZELEC"),
-#         ("medic", "RATOWNIK"),
-#         ("other", "INNY"),
-#     )
-#     name = models.TextField(max_length=255, choices=CHOICES)
-#     user = models.ManyToManyField(MilPerson, related_name='mil_person')
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, null=True, blank=True, related_name='credentials')
+    function_on_board = models.TextField(max_length=255, choices=CHOICES, blank=True, null=True)
 
+    def __str__(self):
+        return f'{self.rank} {self.last_name.upper()} {self.first_name}'
+
+
+# class Pilot(models.Model):
+#     class Meta:
+#         verbose_name_plural = "Piloci"
+#     id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+#     user = models.ManyToManyField(related_name='func_on_board', to=MilPerson)
+
+class Aircraft(models.Model):
+    class Meta:
+        # verbose_name = 'Funkcja'
+        verbose_name_plural = 'Śmigłowce'
+
+    AC_TYPES = (
+        ("w3", "W-3"),
+        # ("mi2", "Mi-2"),
+    )
+    W3_NUMBERS = (
+        ('0901', '0901'),
+        ('0819', '0819'),
+        ('0820', '0820'),
+        ('0615', '0615'),
+        ('0609', '0609'),
+    )
+    aircraft_type = models.TextField(max_length=255, default="w3", choices=AC_TYPES, blank=True, null=True)
+    aircraft_number = models.CharField(max_length=30, blank=False)
 
 
 
