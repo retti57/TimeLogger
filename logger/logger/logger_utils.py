ae0772ea435cd29de230b5eed1e50101b6c3b335
Czaja <dczajka1@gmail.com>
2024-05-15 21:51:54 +0200

    test obliczania czasu

--- logger/logger/logger_utils.py
+++ logger/logger/logger_utils.py
@@ -1,12 +1,13 @@
 from collections import namedtuple
 from datetime import datetime
+from django.db import models
 
 
 class TimeCalculation:
-    def __init__(self, object):
-        self.object = object
+    def __init__(self, model_object: [models.Model, models.Field]):
+        self.object = model_object
 
-    def _time_calculation(self,time1: datetime, time2: datetime) -> float():
+    def _time_calculation(self, time1: datetime, time2: datetime) -> float():
         """ Time calculation of given values of type 'datetime'.
         First value should be earlier value in time concept
         :return: time value in format  HH:MM """
@@ -19,8 +20,7 @@ class TimeCalculation:
 
         return duration.total_seconds()
 
-
-    def _conv_sec_to_H_M(self,totalseconds: float):
+    def _conv_sec_to_H_M(self, totalseconds: float):
         from math import floor
         h = floor(totalseconds / 3600)
         m = floor((totalseconds % 3600) / 60)
@@ -34,13 +34,12 @@ class TimeCalculation:
     def get_times(self):
         """ Returns namedtuple with fields 'air','gnd', 'full'.
         Times in specific fields are converted in format HH:MM"""
-        air = self._conv_sec_to_H_M(self._time_calculation(self.object.start_up, self.object.land))
+        air = self._conv_sec_to_H_M(self._time_calculation(self.object.take_off, self.object.land))
         gnd1 = self._time_calculation(self.object.start_up, self.object.take_off)
         gnd2 = self._time_calculation(self.object.land, self.object.shut_down)
         gnd = gnd2 + gnd1
         gnd_total = self._conv_sec_to_H_M(gnd)
         full = self._conv_sec_to_H_M(self._time_calculation(self.object.start_up, self.object.shut_down))
 
-        FlightTimes = namedtuple("FlightTimes","air gnd full")
+        FlightTimes = namedtuple("FlightTimes", "air gnd full")
         return FlightTimes(air, gnd_total, full)
-
--- logger/time_logger_backend_app/tests.py
+++ logger/time_logger_backend_app/tests.py
@@ -1,23 +1,74 @@
-# from datetime import datetime
-# from logger.logger.logger_utils import conv_sec_to_H_M,time_calculation
-#
-# datetime_values = [
-#     datetime(2023, 7, 27, 15, 15, 0),
-#     datetime(2023, 7, 27, 17, 10, 0),
-#     datetime(2023, 7, 26, 17, 10, 0),
-#     datetime(2023, 7, 25, 17, 10, 0)
-# ]
-#
-#
-# def time_calc():
-#
-#     time1 = datetime_values[2]
-#     time2 = datetime_values[0]
-#
-#     return time_calculation(time1, time2)
-#
-#
-# tot = time_calc()
-# c = conv_sec_to_H_M(tot)
-#
-# def
+from datetime import datetime
+from logger.logger.logger_utils import TimeCalculation
+from collections import namedtuple
+
+
+ModelObject = namedtuple('ModelObject', 'start_up take_off land shut_down')
+
+
+class Calculator(TimeCalculation):
+    """ This class overrides init method of inherited class in order to use simpler object containing
+    datetime attributes """
+    def __init__(self, myobject: ModelObject):
+        self.object = myobject
+
+
+class TestUtilsCalculation:
+
+    def test_fulltime_below_1hour(self):
+        model_object = Calculator(
+            ModelObject(
+                start_up=datetime(2023, 7, 25, 17, 10, 0),
+                take_off=datetime(2023, 7, 25, 17, 20, 0),
+                land=datetime(2023, 7, 25, 17, 50, 0),
+                shut_down=datetime(2023, 7, 25, 17, 55, 0)
+            )
+        )
+        air, ground, full = model_object.get_times()
+        assert full == '00:45'
+        assert ground == '00:15'
+        assert air == '00:30'
+
+    def test_fulltime_equal_1hour(self):
+        model_object = Calculator(
+            ModelObject(
+                start_up=datetime(2023, 7, 25, 17, 10, 0),
+                take_off=datetime(2023, 7, 25, 17, 20, 0),
+                land=datetime(2023, 7, 25, 17, 50, 0),
+                shut_down=datetime(2023, 7, 25, 18, 10, 0)
+            )
+        )
+        air, ground, full = model_object.get_times()
+        assert full == '01:00'
+        assert ground == '00:30'
+        assert air == '00:30'
+
+    def test_fulltime_over_1hour(self):
+        model_object = Calculator(
+            ModelObject(
+                start_up=datetime(2023, 7, 25, 17, 10, 0),
+                take_off=datetime(2023, 7, 25, 17, 20, 0),
+                land=datetime(2023, 7, 25, 18, 45, 0),
+                shut_down=datetime(2023, 7, 25, 18, 55, 0)
+            )
+        )
+        air, ground, full = model_object.get_times()
+        assert full == '01:45'
+        assert ground == '00:20'
+        assert air == '01:25'
+
+    def test_time_over_midnight(self):
+        model_object = Calculator(
+            ModelObject(
+                start_up=datetime(2023, 7, 25, 23, 40, 0),
+                take_off=datetime(2023, 7, 25, 23, 45, 0),
+                land=datetime(2023, 7, 26, 00, 10, 0),
+                shut_down=datetime(2023, 7, 26, 00, 15, 0)
+            )
+        )
+        air, ground, full = model_object.get_times()
+        assert full == '00:35'
+        assert ground == '00:10'
+        assert air == '00:25'
+
+
