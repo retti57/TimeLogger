53d8626133ba688cf36a7ceacd393523b426eee3
Czaja <dczajka1@gmail.com>
2024-08-16 13:14:59 +0200

    Django-unfold w panelu admina.

--- logger/logger/settings.py
+++ logger/logger/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -27,10 +26,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'unfold',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'logger.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -87,7 +85,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -107,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -119,7 +115,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
@@ -129,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+UNFOLD = {
+    "SITE_HEADER": "Logger",
+    "SITE_URL": "/logger/"
+}
--- logger/time_logger_backend_app/admin.py
+++ logger/time_logger_backend_app/admin.py
@@ -1,7 +1,36 @@
 from django.contrib import admin
 from .models import MilPerson, Aircraft, Log, Notes
-# Register your models here.
-admin.site.register(MilPerson)
-admin.site.register(Aircraft)
-admin.site.register(Log)
-admin.site.register(Notes)
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.models import User, Group
+from unfold.forms import UserChangeForm, UserCreationForm, AdminPasswordChangeForm
+from unfold.admin import ModelAdmin
+
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+    pass
+
+
+@admin.register(MilPerson)
+@admin.register(Aircraft)
+@admin.register(Log)
+@admin.register(Notes)
+class CustomAdminClass(ModelAdmin):
+    pass
+
+# admin.site.register(MilPerson)
+# admin.site.register(Aircraft)
+# admin.site.register(Log)
+# admin.site.register(Notes)
